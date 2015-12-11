28079e01682163895338f9391d855803784f15a3
xiaowangren <wangren_0626@126.com>
2015-12-11 09:55:51 +0800

    OK

--- view/MyCommissionTrack.controller.js
+++ view/MyCommissionTrack.controller.js
@@ -1,4 +1,31 @@
 sap.ui.controller("com.zjenergy.bpm.inbox.view.MyCommissionTrack", {
+    onInit:function(){
+        var sServiceUrl = "/sap/opu/odata/SAP/ZBPMHIS_SRV/";
+        var oModel = new sap.ui.model.json.JSONModel();
+        var startProcess = this.getView().byId("startProcess");
+        $.ajax({
+            url: sServiceUrl+"BPMMYHISSet/$count?$filter=IDays eq 30 and IStarted eq 'X'",
+                success: function (data) {
+                    oModel.setData({
+                    	startValue:data
+                    });
+                }
+        }); 
+        startProcess.setModel(oModel);
+        var oModelSp = new sap.ui.model.json.JSONModel();
+        var shenPiProcess = this.getView().byId("shenPiProcess");
+        $.ajax({
+            url: sServiceUrl+"BPMMYHISSet/$count?$filter=IDays eq 30 and IReleased eq 'X'",
+                success: function (data) {
+                    oModelSp.setData({
+                    	shenPiValue:data
+                    });
+                }
+        }); 
+        shenPiProcess.setModel(oModelSp);
+        
+        
+    },
     onPress:function(oEvent){
         var context = oEvent.getSource().getBindingContext();
         sap.ui.getCore().byId("idApp").app.to("idCommissionTrack");
